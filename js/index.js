ccf77b1835fedb714294a7d85f190bf4ebb67a00
Nickely Mayara <138811417+NickelyMayara@users.noreply.github.com>
2024-03-18 11:31:21 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
- 
+ window.addEventListener("scroll", () => {
+    let headerRolagem = document.querySelector('#headerInicial')
+    headerRolagem.classList.toggle('rolagem', window.scrollY > 0)
+ })
--- js/personagens.js
+++ js/personagens.js
@@ -1,9 +1,9 @@
 const urlBase = 'https://api.disneyapi.dev'  // url base da documentação
-const page = 156
+const page = 160
 
 const carregaCaracteres = async () => {
     const res = await fetch(`${urlBase}/character?pageSize=${page}`);
-    const { data } = await res.json();  // para extrair 'data' do objeto retornado
+    const { data} = await res.json();  // para extrair 'data' do objeto retornado
     return data;  // Retorna data diretamente sem precisar colocar em um objeto 
 };
 
@@ -19,29 +19,32 @@ function InfoPersonagens(characters) {  // função das informações de charact
     const characterContainer = document.getElementById('character-container')
     characters.map((character) => {
         const divCharacter = document.createElement('div')
-        divCharacter._id = `character-${character._id}`
+        divCharacter.id = `character-${character.id}`
         divCharacter.innerHTML = `
+        <div class="detalhes-click-personagens">
             <img id="img-character" class="img-character" src="${character.imageUrl}" alt="Imagem do personagem">
             <article class="characterInfo">
                 <h3>${character.name}</h3>
-                `;
-
+            </article>
+        </div>   
+            `;
         divCharacter.classList.add('character-box')
         characterContainer.appendChild(divCharacter) // elemento_pai.appendChild(elemento_filho)
         divCharacter.addEventListener('click', () => {  // ao clicar vai chamar a função
-            detalhesPersonagens(character._id)
+            divCharacter.id = `character-${character.id}`
+            divCharacter.innerHTML = `
+            <div class="detalhes-click-personagens">
+                <img id="img-character" class="img-character" src="${character.imageUrl}" alt="Imagem do personagem">
+                    <article class="characterInfoClick">
+                        <h3 class="texto-info-click">• NOME: ${character.name}</h3>
+                        <h4 class="texto-info-click">• FILME: ${character.films}</h4>
+                        <h4 class="texto-info-click">• JOGOS: ${character.videoGames}</h4>
+                        <h5 class="texto-info-click">• FANDOM: <a href="${character.sourceUrl}">${character.sourceUrl}</a></h5>
+                    </article>
+            </div>
+            `;
+            'character-box'.style.backgroundColor = "#FFF"
         })
     })
 }
-
-function detalhesPersonagens(_id){
-    const idCriptografado = criptografarId(_id)
-    window.location.href = `./detalhes.html?id=${idCriptografado}`
-}
-
-function criptografarId(_id){
-    return _id.toString(36)
-}
-
-
 
